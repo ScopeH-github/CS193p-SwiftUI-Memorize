a99681a60623662eefac60f67ad8b28b90f91ab9
Scope.h <hongsch07@naver.com>
2021-08-03 11:51:30 +0900

    --- temp Commit ---
    Working Copy의 문제로 인한 임시 커밋

--- MemorizeGame.playground/Contents.swift
+++ MemorizeGame.playground/Contents.swift
@@ -3,16 +3,24 @@ import PlaygroundSupport
 
 struct ContentView: View {
     var body: some View {
-        ZStack {
-            RoundedRectangle(cornerRadius: 20)
-                .stroke(lineWidth: 3)
-            Text("Hello, World!")
+        HStack {
+            CardView()
+            CardView()
         }
         .padding(.horizontal)
         .foregroundColor(.orange)
     }
 }
 
+struct CardView: View {
+    var body: some View {
+        ZStack {
+            RoundedRectangle(cornerRadius: 20)
+                .stroke(lineWidth: 3)
+            Text("Hello, World!")
+        }
+    }
+}
 
 
 
