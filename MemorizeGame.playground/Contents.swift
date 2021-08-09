58f497088f066422d6630451db628773075ffabc
Scope.h <hongsch07@naver.com>
2021-08-09 21:53:55 +0900

    Updated Contents.swift
    VStack 제거함

--- MemorizeGame.playground/Contents.swift
+++ MemorizeGame.playground/Contents.swift
@@ -7,14 +7,12 @@ struct ContentView: View {
     let viewModel: EmojiMemoryGame
     
     var body: some View {
-        VStack {
-            ScrollView {
-                
-                LazyVGrid(columns: [GridItem(.adaptive(minimum: 80))]) {
-                    ForEach(viewModel.cards) { card in
-                        CardView(card: card)
-                            .aspectRatio(2/3, contentMode: .fit)
-                    }
+        ScrollView {
+            
+            LazyVGrid(columns: [GridItem(.adaptive(minimum: 80))]) {
+                ForEach(viewModel.cards) { card in
+                    CardView(card: card)
+                        .aspectRatio(2/3, contentMode: .fit)
                 }
             }
         }
